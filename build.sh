21df4a9b58e3379a931126666effb797ebe4575b
zhang.xianfu <zhang.xianfu@tslsmart.com>
2022-09-21 17:42:25 +0800

    适配aarch64 on TX2

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
-cmake -B ./build -DCMAKE_BUILD_TYPE=Release --install-prefix $PWD/miniocpp-sdk/release #-DCMAKE_TOOLCHAIN_FILE=/home/zhangxianfu/Code/vcpkg/scripts/buildsystems/vcpkg.cmake 
-cmake --build ./build --config Release -j
-cmake --install build/
+set -e
 
-cmake -B ./build -DCMAKE_BUILD_TYPE=Debug -G Ninja --install-prefix $PWD/miniocpp-sdk/debug # -DCMAKE_TOOLCHAIN_FILE=/home/zhangxianfu/Code/vcpkg/scripts/buildsystems/vcpkg.cmake 
-cmake --build ./build --config Debug -j
+ENV_ARGS="CC=/usr/bin/gcc-8 CXX=/usr/bin/g++-8"
+apt install -y libcurl4-openssl-dev libcurlpp-dev
+
+eval $ENV_ARGS cmake -B ./build -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_LIBDIR=$PWD/miniocpp-sdk/release --install-prefix $PWD/miniocpp-sdk/release -DCMAKE_TOOLCHAIN_FILE=./vcpkg-master/scripts/buildsystems/vcpkg.cmake -DCMAKE_CXX_LINKER_FLAGS_INIT=-lstdc++fs 
+cmake --build ./build --config Release -j4
+cmake --install build/
+cp -r build/vcpkg_installed/arm64-linux/include $PWD/miniocpp-sdk/release
+cp -r build/vcpkg_installed/arm64-linux/lib $PWD/miniocpp-sdk/release
+cp -r build/vcpkg_installed/arm64-linux/share $PWD/miniocpp-sdk
+
+
+eval $ENV_ARGS cmake -B ./build -DCMAKE_BUILD_TYPE=Debug -G Ninja -DCMAKE_INSTALL_LIBDIR=$PWD/miniocpp-sdk/debug --install-prefix $PWD/miniocpp-sdk/debug -DCMAKE_TOOLCHAIN_FILE=./vcpkg-master/scripts/buildsystems/vcpkg.cmake
+cmake --build ./build --config Debug -j4
 cmake --install build/ 
 
+cp -r build/vcpkg_installed/arm64-linux/include $PWD/miniocpp-sdk/debug
+cp -r build/vcpkg_installed/arm64-linux/debug/lib $PWD/miniocpp-sdk/debug
 
 tar -zcvf miniocpp-sdk.tar.gz miniocpp-sdk
